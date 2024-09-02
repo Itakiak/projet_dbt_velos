0033851013619a66ea4ff12f9f5495602a1a5aa6
Anna Kelejian <akelejian@outlook.com>
2024-09-02 15:54:37 +0000

    mar vdef data meteo paris et lyon

---
+++ models/03-mart/mar__data_meteo_paris.sql
@@ -0,0 +1,29 @@
+WITH source_meteo AS (
+    SELECT * FROM {{ ref("int__data_meteo_paris") }}
+),
+
+source_compteurs AS (
+    SELECT * FROM {{ ref('stg__compteurs_paris') }}
+),
+
+joined_data AS (
+    SELECT
+        c.compt_date_YM,
+        c.id_compteur,
+        c.nom_compteur,
+        c.lat_long,
+        c.velos_count,
+        m.meteo_date_YM,
+        m.avg_windspd_kmh,
+        m.avg_precip_mm,
+        m.avg_heatindex,
+        m.avg_snow_mm,
+        m.avg_temp,
+        m.avg_windchill
+    FROM source_compteurs c
+    JOIN source_meteo m ON c.compt_date_YM = m.meteo_date_YM
+)
+
+SELECT *
+FROM joined_data
+ORDER BY meteo_date_YM, id_compteur
