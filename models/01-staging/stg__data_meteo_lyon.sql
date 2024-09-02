17b63edd7e426b6fda95dea4ead79135510181ac
Anna Kelejian <akelejian@outlook.com>
2024-09-02 14:10:13 +0000

    modif table meteo paris

--- models/01-staging/stg__data_meteo_lyon.sql
+++ models/01-staging/stg__data_meteo_lyon.sql
@@ -4,19 +4,16 @@ with
 
     renamed as (
 
-        select 
-        parse_date('%F', date) as date_formated,
-        FORMAT_DATETIME('%B', parse_date('%F', date)) AS annee_mois,
-        # round(avg(windspeed_max_kmh),2),
-        # round(avg(precip_total_day_mm),2),
-        # round(avg(heatindex_max_c),2),
-        # round(avg(total_snow_mm),2),
-        # round(avg(avg_temp_day),2),
-        # round(avg(windchill),2)
+        select
+            parse_date('%F', date) as date,
+            windspeed_max_kmh,
+            precip_total_day_mm,
+            heatindex_max_c,
+            total_snow_mm,
+            avg_temp_day,
+            windchill
 
-        GROUP BY annee_mois
-        ORDER BY annee_mois
-        
+
         from source
 
     )
