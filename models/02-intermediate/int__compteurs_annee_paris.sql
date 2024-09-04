b4d06463d0879a99702d11e576e83495ec4ffa90
Camille Vacant <camille.vacant@gmail.com>
2024-09-04 13:42:19 +0000

    format dates

--- models/02-intermediate/int__compteurs_annee_paris.sql
+++ models/02-intermediate/int__compteurs_annee_paris.sql
@@ -5,7 +5,7 @@ with
     compteurs_annee as (
 
         select
-            format_date('%Y', compt_date_ym) as compt_year,
+            PARSE_DATE('%Y-%m-%d', CONCAT(format_date('%Y', compt_date_ym), '-01-01')) AS compt_year,
             cast(
                 round(sum(velos_count) / count(distinct id_compteur)) as int64
             ) as avg_compt
